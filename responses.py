cd1505d6db68fd65f11d9cc493383b39fad727bd
Veginite <veginite@gmail.com>
2024-03-26 15:55:08 +0100

    init

--- responses.py
+++ responses.py
@@ -1,10 +1,11 @@
 from scrape import scrape_private_leagues
 from league_count import get_league_count
 
+
 def get_response(user_input: str) -> str:
     prefix = user_input[0]
     if prefix == '§':
-        # content must consist of exactly two components, separated by a white space, following structure §[command] [value]
+        # must follow the exact structure: §[command] [value]
         content_components = user_input.replace('§', '').split(' ')
         if len(content_components) == 2:
             command = content_components[0]
@@ -16,9 +17,10 @@ def get_response(user_input: str) -> str:
                     # scrape data by verifying and using the link in var: val
                     private_leagues: list = scrape_private_leagues(val)
                     private_league_count: int = get_league_count(private_leagues)
-                    return 'You have participated in Conflux leagues during ' + str(private_league_count) + ' unique years'
+                    return 'You have participated in Conflux leagues during ' + str(
+                        private_league_count) + ' unique years'
                 case _:
                     return 'Unknown command: "' + command + '", see pinned messages for instructions!'
-            #-------- end command handling --------
+            # -------- end command handling --------
         else:
             return 'Invalid command structure: "' + user_input + '", command must follow structure: §[command] [value]'
--- scrape.py
+++ scrape.py
@@ -1,6 +1,4 @@
 import cloudscraper
-import requests
-from requests import Response
 from lxml import etree
 from io import StringIO
 
@@ -9,13 +7,9 @@ def scrape_private_leagues(account_name: str) -> list:
     parser = etree.HTMLParser()
     url = 'https://www.pathofexile.com/account/view-profile/' + account_name + '/private-leagues'
 
+    # error handling needs to be implemented here (404, 503)
     scraper = cloudscraper.create_scraper()
-    page: Response
-    try:
-        page = scraper.get(url)
-    except requests.exceptions.RequestException as e:
-        print(e)
-        return []
+    page = scraper.get(url)
 
     html = page.content.decode('utf-8')
     tree = etree.parse(StringIO(html), parser=parser)
