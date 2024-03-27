70d08913238bf1ac9b8904c0b311232103806736
Veginite <veginite@gmail.com>
2024-03-27 07:34:47 +0100

    full support for paginated scraping

--- scrape.py
+++ scrape.py
@@ -1,6 +1,8 @@
 import cloudscraper
 from lxml import etree
 from io import StringIO
+import math
+import time
 
 
 def scrape_private_leagues(account_name: str) -> list:
@@ -14,5 +16,23 @@ def scrape_private_leagues(account_name: str) -> list:
     html = page.content.decode('utf-8')
     tree = etree.parse(StringIO(html), parser=parser)
 
+    pagination_count = get_pagination_count(tree)
+
+    leagues = tree.getroot().findall('.//*[@class="custom-league-list"]/div')
+    for n in range(pagination_count - 1):
+        p_url = url + '?page=' + str(n + 2)  # page number is counter index offset by 2
+        page = scraper.get(p_url)
+        html = page.content.decode('utf-8')
+        tree = etree.parse(StringIO(html), parser=parser)
+        leagues += tree.getroot().findall('.//*[@class="custom-league-list"]/div')
+        time.sleep(0.25)  # delay the next https request to prevent a potential cloudflare bot flag
+
+    return leagues
+
+
+# used to scrape multiple private league pages
+def get_pagination_count(tree: etree) -> int:
     root = tree.getroot()
-    return root.findall('.//*[@class="custom-league-list"]/div')
+    e = root.find('.//div[@class="total-count"]')
+    return math.ceil(
+        int(e.text.split(': ', 1)[1]) / 20)  # the amount of PL pages are the total PL count / 20 rounded up
