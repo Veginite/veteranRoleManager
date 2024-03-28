345df1a373ce76f9874be3909731e1f4f0f025da
Veginite <veginite@gmail.com>
2024-03-28 03:40:36 +0100

    added support for holes in role hierarchy

--- assign_user_veteran_role.py
+++ assign_user_veteran_role.py
@@ -4,6 +4,7 @@ from lxml import etree
 
 async def assign_user_veteran_role(conflux_year_count: int, user: discord.Message.author) -> str:
     msg: str = ''
+    max_veteran_role_count: int = 4
 
     server = user.guild
     tree = etree.parse('roles.xml')
@@ -12,15 +13,14 @@ async def assign_user_veteran_role(conflux_year_count: int, user: discord.Messag
     for e in root:
         role_ids.append(int(e.attrib["id"]))
 
-    matches: int = 0
-    for role in user.roles:
-        if role.id in role_ids:
-            matches += 1
+    # find the roles the user already has and exclude them, mainly to support potential role hierarchy holes
+    matches = [x.id for x in user.roles if x.id in role_ids]
+    for n in range(len(matches)):
+        role_ids.remove(matches[n])
 
     # maximum existing veteran roles are 4
-    n_roles_to_add = min(conflux_year_count, 4) - matches
-
-    if matches == 4:
+    n_roles_to_add = min(conflux_year_count, max_veteran_role_count) - (4 - len(role_ids))
+    if len(role_ids) == 0:
         return 'The only thing beyond 4 roles is NON-EXISTENCE!'
     elif n_roles_to_add == 0:
         return 'You will be eligible for a new role next year!'
@@ -30,8 +30,8 @@ async def assign_user_veteran_role(conflux_year_count: int, user: discord.Messag
         conflux_year_count) + ' years, <@' + str(user.id) + '> has been awarded the following: '
 
     for n in range(n_roles_to_add):
-        s_role = server.get_role(role_ids[matches + n])
+        s_role = server.get_role(role_ids[n])
         await user.add_roles(s_role)
-        msg += '<@&' + str(role_ids[matches + n]) + '>'
+        msg += '<@&' + str(role_ids[n]) + '>'
 
     return msg
