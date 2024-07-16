7f5413ee0593de420ee25076f84e2beaf8165b01
Veginite <veginite@gmail.com>
2024-07-16 17:50:59 +0200

    added asynchronous support for queuing messages and handling them by delayed intervals

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
+import asyncio
 from typing import Final
 import os
 from dotenv import load_dotenv
 from discord import Intents, Client, Message
 from responses import get_response
 from dbc import create_server_connection
+from queue import Queue
+from discord.ext import tasks
+import datetime as dt
+import pytz
 
 load_dotenv()
 TOKEN: Final[str] = os.getenv("DISCORD_TOKEN")
-DBPASS: Final[str] = os.getenv("DBPASS")
+#DBPASS: Final[str] = os.getenv("DBPASS")
 
 intents: Intents = Intents.default()
 intents.message_content = True
 client: Client = Client(intents=intents)
-dbc = create_server_connection("localhost", "root", DBPASS)
+#dbc = create_server_connection("localhost", "root", DBPASS)
+q: Queue = Queue()
 
 
 async def send_message(message: Message, user_message: str) -> None:
@@ -24,26 +30,39 @@ async def send_message(message: Message, user_message: str) -> None:
     try:
         response = await get_response(user_message, message.author)
         await message.channel.send(response)
+
     except Exception as e:
         print(e)
 
 
 @client.event
 async def on_ready() -> None:
+    handle_message_queue.start()
     print(f'{client.user} is now running!')
 
 
 @client.event
 async def on_message(message: Message) -> None:
-    # prevents a potential infinite message loop
+    # Prevents a potential infinite message loop
     if message.author == client.user:
         return
 
-    username = str(message.author)
-    user_message = str(message.content)
-    channel = str(message.channel)
+    q.put(message)
 
-    await send_message(message, user_message)
+    # Only handling minutes and seconds will save the hassle of considering time zones and incorrect hours
+    nextInterval = handle_message_queue.next_iteration.minute * 60 + handle_message_queue.next_iteration.second
+    now = dt.datetime.now().minute * 60 + dt.datetime.now().second
+    eta = (nextInterval - now + ((q.qsize() - 1) * handle_message_queue.seconds))
+
+    await message.channel.send(f'There are currently {q.qsize()} requests queued. ETA is {eta} seconds.'
+                               + f' Additional delay will incur if your profile has more than one private league page.')
+
+
+@tasks.loop(seconds=20)
+async def handle_message_queue():
+    if not q.empty():
+        message = q.get()
+        await send_message(message, message.content)
 
 
 def main() -> None:
