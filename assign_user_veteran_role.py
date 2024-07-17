bd6674fcce3ace9e3df878e388cb3cb1ffbde451
Veginite <veginite@gmail.com>
2024-07-17 02:55:54 +0200

    added support for expansion of roles and changed the way roles are managed to assign the highest possible role and remove irrelevant ones

--- assign_user_veteran_role.py
+++ assign_user_veteran_role.py
@@ -4,33 +4,39 @@ from lxml import etree
 
 async def assign_user_veteran_role(conflux_year_count: int, user: discord.Message.author) -> str:
     msg: str = ''
-    max_veteran_role_count: int = 4
 
     server = user.guild
     tree = etree.parse('roles.xml')
     root = tree.getroot()
     role_ids: list = []
+    role_names: list = []
     for e in root:
         role_ids.append(int(e.attrib["id"]))
+        role_names.append(str(e.attrib["name"]))
 
-    # find the roles the user already has and exclude them, mainly to support potential role hierarchy holes
-    matches = [x.id for x in user.roles if x.id in role_ids]
-    for n in range(len(matches)):
-        role_ids.remove(matches[n])
+    # the number of roles present in roles.xml
+    max_veteran_role_count: int = len(role_ids)
 
-    # maximum existing veteran roles are 4
-    n_roles_to_add = min(conflux_year_count, max_veteran_role_count) - (4 - len(role_ids))
-    if len(role_ids) == 0:
-        return 'The only thing beyond 4 roles is NON-EXISTENCE!'
-    elif n_roles_to_add == 0:
+    # conflux_year_count may never exceed max_veteran_role_count
+    conflux_year_count = min(conflux_year_count, max_veteran_role_count)
+
+    # if the top role has already been awarded, sass
+    if role_ids[max_veteran_role_count - 1] in user.roles:
+        return f'The only thing beyond {role_names[max_veteran_role_count - 1]} roles is NON-EXISTENCE!'
+    # if the eligible role has already been awarded, be kind
+    elif role_ids[conflux_year_count - 1] in user.roles:
         return 'You will be eligible for a new role next year!'
+    # the user is eligible for the relevant role, award it and remove all other roles
+    else:
+        # remove a prior role if they have it
+        for n in range(conflux_year_count - 1):
+            if role_ids[n] in user.roles:
+                await user.remove_roles(server.get_role(role_ids[n]))
 
-    msg = (f'For playing Conflux during {str(conflux_year_count)} years, '
-           f'<@{str(user.id)}> has been awarded the following: ')
+        msg = (f'For playing Conflux during {str(conflux_year_count)} years, '
+               f'<@{str(user.id)}> has been awarded the following: ')
 
-    for n in range(n_roles_to_add):
-        s_role = server.get_role(role_ids[n])
-        await user.add_roles(s_role)
-        msg += f'<@&{str(role_ids[n])}>'
+        await user.add_roles(server.get_role(role_ids[conflux_year_count - 1]))
+        msg += f'<@&{str(role_ids[conflux_year_count - 1])}>'
 
     return msg
