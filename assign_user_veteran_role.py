45667d14c44b7932ef868c28603c9590a67f21dd
Veginite <veginite@gmail.com>
2024-07-17 23:06:32 +0200

    final bug fixes

--- assign_user_veteran_role.py
+++ assign_user_veteran_role.py
@@ -17,26 +17,27 @@ async def assign_user_veteran_role(conflux_year_count: int, user: discord.Messag
     # the number of roles present in roles.xml
     max_veteran_role_count: int = len(role_ids)
 
-    # conflux_year_count may never exceed max_veteran_role_count
+    # highest role based on conflux_year_count may never exceed max_veteran_role_count
     highest_role_to_assign = min(conflux_year_count, max_veteran_role_count)
 
+    user_veteran_roles = [x.id for x in user.roles if x.id in role_ids]
+
     # if the top role has already been awarded, sass
-    if highest_role_to_assign == max_veteran_role_count and [max_veteran_role_count] in user.roles:
+    if highest_role_to_assign == max_veteran_role_count and role_ids[max_veteran_role_count - 1] in user_veteran_roles:
         msg = f'The only thing beyond {role_names[max_veteran_role_count - 1]} roles is NON-EXISTENCE!'
     # if the eligible role has already been awarded, be kind
-    elif role_ids[highest_role_to_assign - 1] in user.roles:
+    elif role_ids[highest_role_to_assign - 1] in user_veteran_roles:
         msg = 'You will be eligible for a new role next year!'
     # the user is eligible for the relevant role, award it and remove all other roles
     else:
-        # remove a prior role if they have it
-        for n in range(highest_role_to_assign - 1):
-            if role_ids[n] in user.roles:
-                await user.remove_roles(server.get_role(role_ids[n]))
+        # remove prior roles
+        for n in range(len(user_veteran_roles)):
+            await user.remove_roles(server.get_role(user_veteran_roles[n]))
 
         msg = (f'For playing Conflux during {str(highest_role_to_assign)} years, '
                f'<@{str(user.id)}> has been awarded the following: ')
+        msg += f'<@&{str(role_ids[highest_role_to_assign - 1])}>'
 
         await user.add_roles(server.get_role(role_ids[highest_role_to_assign - 1]))
-        msg += f'<@&{str(role_ids[highest_role_to_assign - 1])}>'
 
     return msg
